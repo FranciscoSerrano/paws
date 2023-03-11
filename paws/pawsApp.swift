f2364929bb7b64fbacdba7cf7f856414449559e0
Francisco Serrano <fserrano@apple.com>
2023-03-11 13:20:27 -0800

    Initial Commit

---
+++ paws/pawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pawsApp.swift
+//  paws
+//
+//  Created by Francisco Serrano on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct pawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
